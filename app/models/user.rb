6a62ff22d7437d43b3f8610b72fadb96fc7dd882
初コミット <tagamitomohiro@tagamitomohiro-no-MacBook-Pro.local>
2016-05-04 16:25:38 +0900

    match_color雛形(proof参照)

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>"}
+  validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png"]
+
+
+  def name
+    "#{family_name} #{first_name}"
+  end
+
+  def name_kana
+    "#{family_name_kana} #{first_name_kana}"
+  end
+
+  def full_profile?
+    avatar? && family_name? && first_name? && family_name_kana? && first_name_kana?
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:show, :edit, :update]
+  root "top#index"
+end
